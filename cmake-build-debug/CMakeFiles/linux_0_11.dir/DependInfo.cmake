ecd7f3584b2ed96c7ae18eba39a60188ef523c2e
Kobe Wang <kobe_wang@126.com>
2020-01-07 13:44:02 +0800

    v0.1 original code

---
+++ cmake-build-debug/CMakeFiles/linux_0_11.dir/DependInfo.cmake
@@ -0,0 +1,75 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/bitmap.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/bitmap.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/block_dev.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/block_dev.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/buffer.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/buffer.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/char_dev.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/char_dev.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/exec.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/exec.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/fcntl.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/fcntl.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/file_dev.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/file_dev.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/file_table.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/file_table.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/inode.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/inode.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/ioctl.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/ioctl.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/namei.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/namei.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/open.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/open.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/pipe.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/pipe.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/read_write.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/read_write.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/stat.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/stat.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/super.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/super.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/fs/truncate.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/fs/truncate.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/init/main.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/init/main.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/blk_drv/floppy.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/blk_drv/floppy.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/blk_drv/hd.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/blk_drv/hd.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/blk_drv/ll_rw_blk.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/blk_drv/ll_rw_blk.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/blk_drv/ramdisk.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/blk_drv/ramdisk.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/chr_drv/console.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/chr_drv/console.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/chr_drv/serial.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/chr_drv/serial.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/chr_drv/tty_io.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/chr_drv/tty_io.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/chr_drv/tty_ioctl.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/chr_drv/tty_ioctl.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/exit.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/exit.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/fork.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/fork.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/math/math_emulate.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/math/math_emulate.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/mktime.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/mktime.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/panic.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/panic.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/printk.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/printk.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/sched.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/sched.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/signal.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/signal.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/sys.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/sys.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/traps.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/traps.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/kernel/vsprintf.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/kernel/vsprintf.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/_exit.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/_exit.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/close.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/close.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/ctype.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/ctype.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/dup.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/dup.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/errno.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/errno.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/execve.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/execve.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/malloc.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/malloc.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/open.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/open.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/setsid.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/setsid.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/string.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/string.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/wait.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/wait.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/lib/write.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/lib/write.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/mm/memory.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/mm/memory.c.o"
+  "/Users/kobewang/CLionProjects/linux-0.11/tools/build.c" "/Users/kobewang/CLionProjects/linux-0.11/cmake-build-debug/CMakeFiles/linux_0_11.dir/tools/build.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/asm"
+  "../include/linux"
+  "../include/sys"
+  "../kernel/blk_drv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
